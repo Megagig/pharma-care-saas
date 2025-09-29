061ddf2f6e5ec6716630062fa21776a37c94ad53
Megagig <megagigsoftwaresolutions@gmail.com>
2025-09-29 19:18:28 +0100

    Root cleanup

--- fix_remaining_errors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix error with catch clause variable type
-sed -i '392s/endpointError: Error/endpointError: unknown/' /home/megagig/PROJECTS/MERN/pharma-care-saas/frontend/src/services/drugInfoApi.ts
-
-# Fix the record type issue by using more specific types
-sed -i '369s/(r: Record<string, any>)/(r: Record<string, unknown>)/' /home/megagig/PROJECTS/MERN/pharma-care-saas/frontend/src/services/drugInfoApi.ts
-
-echo "Fixed remaining TypeScript errors in drugInfoApi.ts"
