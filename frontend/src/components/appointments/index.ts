987040fffe7a9ef66b5f925c75030b473f70ac90
Megagig <megagigsoftwaresolutions@gmail.com>
2025-10-26 07:38:02 +0100

    Build CreateAppointmentDialog component

--- frontend/src/components/appointments/index.ts
+++ frontend/src/components/appointments/index.ts
@@ -1,3 +1,5 @@
 export { default as AppointmentCalendar } from './AppointmentCalendar';
 export { default as CreateAppointmentDialog } from './CreateAppointmentDialog';
 export { default as AppointmentDetailsPanel } from './AppointmentDetailsPanel';
+export { default as PatientAutocomplete } from './PatientAutocomplete';
+export { default as PharmacistSelector } from './PharmacistSelector';
