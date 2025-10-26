642804100b274545bd1e621b54f6117dd1c5db95
Megagig <megagigsoftwaresolutions@gmail.com>
2025-10-26 09:10:25 +0100

    Build RescheduleAppointmentDialog component

--- frontend/src/components/appointments/index.ts
+++ frontend/src/components/appointments/index.ts
@@ -1,6 +1,7 @@
 export { default as AppointmentCalendar } from './AppointmentCalendar';
 export { default as CreateAppointmentDialog } from './CreateAppointmentDialog';
 export { default as CompleteAppointmentDialog } from './CompleteAppointmentDialog';
+export { default as RescheduleAppointmentDialog } from './RescheduleAppointmentDialog';
 export { default as AppointmentDetailsPanel } from './AppointmentDetailsPanel';
 export { default as PatientAutocomplete } from './PatientAutocomplete';
 export { default as PharmacistSelector } from './PharmacistSelector';
