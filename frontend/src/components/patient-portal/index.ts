84ec65adada365b99630ce67eee9bb8bf97c14bc
Megagig <megagigsoftwaresolutions@gmail.com>
2025-10-26 20:41:39 +0100

    Build MyAppointmentsList component

--- frontend/src/components/patient-portal/index.ts
+++ frontend/src/components/patient-portal/index.ts
@@ -1,2 +1,3 @@
 export { default as AvailableSlotsView } from './AvailableSlotsView';
 export { default as BookAppointmentForm } from './BookAppointmentForm';
+export { default as MyAppointmentsList } from './MyAppointmentsList';
